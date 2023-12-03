25f5a48ac2503ec3935b4bbecdac4f262408197f
ritchieculanag <ritchieculanag@gmail.com>
2023-12-03 11:09:51 +0800

    Initial commit

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Register.java
@@ -0,0 +1,16 @@
+package io.github.rmmc.rmmctourism.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.rmmc.rmmctourism.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
