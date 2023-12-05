7e25f934b8e8eff3bb9157bc61156240e7f81c66
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 15:26:20 +0800

    basic configs

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
@@ -3,14 +3,26 @@ package io.github.rmmc.rmmctourism.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import io.github.rmmc.rmmctourism.R;
 
 public class Login extends AppCompatActivity {
 
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.button);
+        btnLogin.setOnClickListener(this::hello);
     }
+
+    private void hello(View view) {
+
+    }
+
 }
