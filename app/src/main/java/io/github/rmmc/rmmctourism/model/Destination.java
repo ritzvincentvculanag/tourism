a792cb757794aaa21161e1873d5e52c000b07116
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 15:54:14 +0800

    update: rename from TouristSpotModel to Destination

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/model/Destination.java
@@ -0,0 +1,13 @@
+package io.github.rmmc.rmmctourism.model;
+
+public class Destination {
+    private final String name;
+
+    public Destination(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
