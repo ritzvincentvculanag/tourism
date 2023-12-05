0c4da5c833d7170c1a719bbebc051e0e436c7a14
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 16:23:47 +0800

    update: implement Parcelable interface

--- app/src/main/java/io/github/rmmc/rmmctourism/model/Destination.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/model/Destination.java
@@ -1,6 +1,14 @@
 package io.github.rmmc.rmmctourism.model;
 
-public class Destination {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Destination implements Parcelable {
+    // Name for parcelable
+    public static final String OBJ_DESTINATION = "OBJ_DESTINATION";
+
     private final String name;
 
     public Destination(String name) {
@@ -9,5 +17,32 @@ public class Destination {
 
     public String getName() {
         return name;
+    }
+
+    // Parcelable details start here
+    protected Destination(Parcel in) {
+        name = in.readString();
+    }
+
+    public static final Creator<Destination> CREATOR = new Creator<Destination>() {
+        @Override
+        public Destination createFromParcel(Parcel in) {
+            return new Destination(in);
+        }
+
+        @Override
+        public Destination[] newArray(int size) {
+            return new Destination[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(name);
     }
 }
