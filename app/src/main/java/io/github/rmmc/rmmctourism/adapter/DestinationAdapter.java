a90072bd6a13e75d401eee859c468636d8c047a8
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 16:23:30 +0800

    update: refactor DestinationsAdapter

--- app/src/main/java/io/github/rmmc/rmmctourism/adapter/DestinationAdapter.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/adapter/DestinationAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,15 +18,17 @@ import java.util.List;
 
 import io.github.rmmc.rmmctourism.R;
 import io.github.rmmc.rmmctourism.model.Destination;
-import io.github.rmmc.rmmctourism.views.TouristSpotInformationActivity;
+import io.github.rmmc.rmmctourism.views.DestinationInformation;
 
 public class DestinationAdapter extends RecyclerView.Adapter<DestinationAdapter.DestinationViewHolder> {
 
     private final Context context;
+    private final OnDestinationClick onDestinationClick;
     private final List<Destination> list;
 
-    public DestinationAdapter(Context context, List<Destination> list){
+    public DestinationAdapter(Context context, OnDestinationClick onDestinationClick, List<Destination> list){
         this.context = context;
+        this.onDestinationClick = onDestinationClick;
         this.list = list;
     }
 
@@ -35,13 +38,13 @@ public class DestinationAdapter extends RecyclerView.Adapter<DestinationAdapter.
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.rv_spot_row, parent, false);
 
-        return new DestinationViewHolder(view);
+        return new DestinationViewHolder(view, onDestinationClick);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DestinationViewHolder holder, int position) {
         holder.cvTouristSpot.setOnClickListener(e ->{
-            context.startActivity(new Intent(this.context, TouristSpotInformationActivity.class));
+            context.startActivity(new Intent(this.context, DestinationInformation.class));
         });
     }
 
@@ -56,14 +59,28 @@ public class DestinationAdapter extends RecyclerView.Adapter<DestinationAdapter.
         private final ImageView imgTouristCover;
         private final TextView tvTouristSpotName;
         private final TextView tvTouristDescription;
+        private final Button btnViewDestination;
 
-        public DestinationViewHolder(@NonNull View itemView) {
+        public DestinationViewHolder(@NonNull View itemView, OnDestinationClick onDestinationClick) {
             super(itemView);
 
             cvTouristSpot = itemView.findViewById(R.id.cv_tourist_spot);
             imgTouristCover = itemView.findViewById(R.id.img_tourist_cover);
             tvTouristSpotName = itemView.findViewById(R.id.tv_tourist_spot_name);
             tvTouristDescription = itemView.findViewById(R.id.tv_tourist_spot_description);
+            btnViewDestination = itemView.findViewById(R.id.btn_view_destination);
+
+            btnViewDestination.setOnClickListener(view -> {
+                if (onDestinationClick == null) {
+                    return;
+                }
+
+                if (getAdapterPosition() == RecyclerView.NO_POSITION) {
+                    return;
+                }
+
+                onDestinationClick.onDestinationClick(getAdapterPosition());
+            });
         }
     }
 }
