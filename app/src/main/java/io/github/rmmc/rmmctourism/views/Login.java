88ee47e3fd3c8990151bc64601955bca4347b7db
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 16:47:06 +0800

    update: remove logic for login

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
@@ -12,53 +12,9 @@ import io.github.rmmc.rmmctourism.R;
 import io.github.rmmc.rmmctourism.util.Validator;
 
  public class Login extends AppCompatActivity {
-
-
-    private TextInputLayout tfUsername;
-    private TextInputLayout tfPassword;
-    private Button btnLogin;
-    private Button btnRegister;
-
-    private FirebaseAuth userAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        tfUsername = findViewById(R.id.tf_username);
-        tfPassword = findViewById(R.id.tf_password);
-
-        btnLogin = findViewById(R.id.btn_login);
-        btnRegister = findViewById(R.id.btn_register);
-
-        btnLogin.setOnClickListener(this::loginListener);
-
-        userAuth = FirebaseAuth.getInstance();
-
-        if(userAuth.getCurrentUser() != null){
-            startActivity(new Intent(this, HomeActivity.class));
-            finish();
-        }
-    }
-    private void loginListener(View view){
-
-        TextInputLayout[] fields = {tfUsername, tfPassword};
-
-        if(!Validator.fieldsAreEmpty(fields)){
-
-            String email = tfUsername.getEditText().getText().toString();
-            String password = tfPassword.getEditText().getText().toString();
-
-            userAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, task ->{
-                        if(task.isSuccessful()){
-                            startActivity(new Intent(this, HomeActivity.class));
-                            finish();
-                        }else{
-
-                        }
-                    });
-        }
     }
 }
