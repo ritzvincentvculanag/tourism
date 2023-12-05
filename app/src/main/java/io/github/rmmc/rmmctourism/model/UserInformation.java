3f50f4b1cb9303743b09348df74424daaebf6949
ladengwapo <domingo.laden@gmail.com>
2023-12-05 22:39:54 +0800

    fix bug

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/model/UserInformation.java
@@ -0,0 +1,18 @@
+package io.github.rmmc.rmmctourism.model;
+
+import java.time.LocalDate;
+
+public class UserInformation {
+
+    private static final String collectionName = "userInformation";
+    private String UID;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate birthDate;
+    private String gender;
+    private String email;
+    private String password;
+
+
+}
