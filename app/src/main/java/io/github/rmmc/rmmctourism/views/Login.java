c30b6b87f3f319731cce239d9275fb017b0c7acd
ritchieculanag <ritchieculanag@gmail.com>
2023-12-05 17:36:27 +0800

    update: add Messenger utility class

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
@@ -1,5 +1,9 @@
  package io.github.rmmc.rmmctourism.views;
 
+import static io.github.rmmc.rmmctourism.util.Messenger.showAlertDialog;
+import static io.github.rmmc.rmmctourism.util.Validator.fieldIsEmpty;
+import static io.github.rmmc.rmmctourism.util.Validator.fieldsAreEmpty;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +13,55 @@ import android.widget.Button;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.util.Messenger;
 import io.github.rmmc.rmmctourism.util.Validator;
 
  public class Login extends AppCompatActivity {
+     private TextInputLayout tilLoginUsername;
+     private TextInputLayout tilLoginPassword;
+
+     private Button btnLogin;
+     private Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeWidgets();
+        initializeListeners();
+    }
+
+    private void initializeWidgets() {
+        tilLoginUsername = findViewById(R.id.til_login_username);
+        tilLoginPassword = findViewById(R.id.til_login_password);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+    }
+
+    private void initializeListeners() {
+        btnLogin.setOnClickListener(this::login);
+        btnRegister.setOnClickListener(this::register);
+    }
+
+    private void login(View view) {
+        if (fieldsAreEmpty(tilLoginUsername, tilLoginPassword)) {
+            showAlertDialog(
+                    this,
+                    "Login Error",
+                    "Username and password is required. Try again."
+            ).show();
+
+            return;
+        }
+
+        Intent goToHome = new Intent(this, HomeActivity.class);
+        startActivity(goToHome);
+    }
+
+    private void register(View view) {
+        Intent gotToRegister = new Intent(this, Register.class);
+        startActivity(gotToRegister);
     }
 }
