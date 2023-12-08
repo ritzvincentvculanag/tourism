3263434186bd513f001278758644208145f92cf2
ritchieculanag <ritchieculanag@gmail.com>
2023-12-08 23:10:32 +0800

    update: extract task successful to separate method reference

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Login.java
@@ -1,31 +1,34 @@
 package io.github.rmmc.rmmctourism.views;
 
 import static io.github.rmmc.rmmctourism.util.Messenger.showAlertDialog;
-import static io.github.rmmc.rmmctourism.util.Validator.fieldIsEmpty;
 import static io.github.rmmc.rmmctourism.util.Validator.fieldsAreEmpty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import io.github.rmmc.rmmctourism.R;
-import io.github.rmmc.rmmctourism.util.Messenger;
-import io.github.rmmc.rmmctourism.util.Validator;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.Miner;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements WidgetInitializer, ActionInitializer {
+
     private TextInputLayout tilLoginUsername;
     private TextInputLayout tilLoginPassword;
 
     private Button btnLogin;
-    private Button btnRegister;
+    private Button btnNotRegistered;
+    private Button btnForgotPassword;
 
     private FirebaseAuth userAuth;
 
@@ -35,56 +38,77 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         initializeWidgets();
-        initializeListeners();
+        initializeActions();
         initializeFirebaseAuth();
     }
 
-    private void initializeWidgets() {
+    @Override
+    public void initializeWidgets() {
         tilLoginUsername = findViewById(R.id.til_login_username);
         tilLoginPassword = findViewById(R.id.til_login_password);
 
         btnLogin = findViewById(R.id.btn_login);
-        btnRegister = findViewById(R.id.btn_register);
+        btnNotRegistered = findViewById(R.id.btn_not_registered);
+        btnForgotPassword = findViewById(R.id.btn_forgot_password);
     }
 
-    private void initializeListeners() {
+    @Override
+    public void initializeActions() {
         btnLogin.setOnClickListener(this::login);
-        btnRegister.setOnClickListener(this::register);
+        btnNotRegistered.setOnClickListener(this::register);
     }
 
     private void initializeFirebaseAuth() {
         userAuth = FirebaseAuth.getInstance();
-
-        if (userAuth.getCurrentUser() != null) {
-            Intent goToHome = new Intent(this, HomeActivity.class);
-            startActivity(goToHome);
+        if (userAuth.getCurrentUser() == null) {
+            return;
         }
+
+        Intent goToHome = new Intent(this, HomeActivity.class);
+        startActivity(goToHome);
     }
 
     private void login(View view) {
         if (fieldsAreEmpty(tilLoginUsername, tilLoginPassword)) {
-            Messenger.showAlertDialog(this, "User Login", "Username and password is required!").show();
+            showAlertDialog(
+                    this,
+                    getString(R.string.login_dialog_error_title),
+                    getString(R.string.login_dialog_error_message),
+                    getString(R.string.login_dialog_error_positive_button_title)
+            ).show();
 
             return;
         }
 
-        String username = tilLoginUsername.getEditText().getText().toString();
-        String password = tilLoginPassword.getEditText().getText().toString();
-
+        String username = Miner.getString(tilLoginUsername);
+        String password = Miner.getString(tilLoginPassword);
         userAuth.signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Intent goToHome = new Intent(this, HomeActivity.class);
-                        startActivity(goToHome);
-                        return;
-                    }
+                .addOnCompleteListener(this, this::loginOnComplete);
+    }
 
-                    Toast.makeText(this, "User not found!", Toast.LENGTH_SHORT).show();
-                });
+    private void loginOnComplete(Task<AuthResult> task) {
+        if (!task.isSuccessful()) {
+            showAlertDialog(
+                    this,
+                    getString(R.string.login_dialog_error_title),
+                    getString(R.string.loging_dialog_error_user_not_found),
+                    getString(R.string.login_dialog_error_positive_button_title)
+            ).show();
+
+            return;
+        }
+
+        Intent goToHome = new Intent(this, HomeActivity.class);
+        startActivity(goToHome);
     }
 
     private void register(View view) {
         Intent gotToRegister = new Intent(this, Register.class);
         startActivity(gotToRegister);
     }
+
+    private void forgotPassword(View view) {
+        // TODO: Implement forgot password
+    }
+
 }
