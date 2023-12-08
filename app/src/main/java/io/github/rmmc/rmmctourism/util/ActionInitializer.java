df73dc15c310ecd6d55499d74119d0aa4df65f79
ritchieculanag <ritchieculanag@gmail.com>
2023-12-08 22:27:11 +0800

    update: add interfaces for initializing actions and widgets

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/util/ActionInitializer.java
@@ -0,0 +1,12 @@
+package io.github.rmmc.rmmctourism.util;
+
+public interface ActionInitializer {
+    /**
+     * Use this method to initialize all the actions associated
+     * with your widgets such as {@code onClick, onDelete}
+     *
+     * @author  Ritchie
+     * @since   December 08, 2023
+     */
+    void initializeActions();
+}
---
+++ app/src/main/java/io/github/rmmc/rmmctourism/util/WidgetInitializer.java
@@ -0,0 +1,12 @@
+package io.github.rmmc.rmmctourism.util;
+
+public interface WidgetInitializer {
+    /**
+     * Use this method to initialize all the widgets
+     * declared in your xml views.
+     *
+     * @author  Ritchie
+     * @since   December 08, 2023
+     */
+    void initializeWidgets();
+}
