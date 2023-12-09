bfe789fa6cbbcc1028d9cdb0cc96f48532f2206e
ritchieculanag <ritchieculanag@gmail.com>
2023-12-09 23:09:07 +0800

    update: initial

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Hero.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Hero.java
@@ -1,16 +1,45 @@
 package io.github.rmmc.rmmctourism.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
-import io.github.rmmc.rmmctourism.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Hero extends AppCompatActivity {
+import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
+
+public class Hero extends AppCompatActivity implements WidgetInitializer, ActionInitializer {
+
+    private BottomNavigationView bnvHero;
+    private NavHostFragment nvfHero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero);
+
+        initializeWidgets();
+        initializeActions();
+    }
+
+    @Override
+    public void initializeActions() {
+
+    }
+
+    @Override
+    public void initializeWidgets() {
+
+
+        initializeBottomNavigation();
+    }
+
+    private void initializeBottomNavigation() {
+        bnvHero = findViewById(R.id.bnvHero);
+        nvfHero = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nvfHero);
     }
 }
