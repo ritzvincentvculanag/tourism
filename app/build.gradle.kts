4b94fd053f9c3521a72f00ad102722f902afa08a
ritchieculanag <ritchieculanag@gmail.com>
2023-12-09 17:44:50 +0800

    update: add navigation component to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.5"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -37,6 +38,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
