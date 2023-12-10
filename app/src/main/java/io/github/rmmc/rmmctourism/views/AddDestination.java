0fce6751e3783a9409c379bb7c68fb647812e3d2
ritchieculanag <ritchieculanag@gmail.com>
2023-12-10 20:07:35 +0800

    update: add initial select cover photo

--- app/src/main/java/io/github/rmmc/rmmctourism/views/AddDestination.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/AddDestination.java
@@ -1,66 +1,60 @@
 package io.github.rmmc.rmmctourism.views;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddDestination#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddDestination extends Fragment {
+public class AddDestination extends Fragment implements WidgetInitializer, ActionInitializer {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btnUploadCover;
 
-    public AddDestination() {
-        // Required empty public constructor
-    }
+    private ActivityResultLauncher<String> arlTakePhoto;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddDestination.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddDestination newInstance(String param1, String param2) {
-        AddDestination fragment = new AddDestination();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_add_destination, container, false);
+
+        initializeWidgets();
+        initializeActions();
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void initializeActions() {
+
+        arlTakePhoto = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        Log.d("RITCHIE", uri.toString());
+                    }
+                }
+        );
+
+        btnUploadCover.setOnClickListener(e -> arlTakePhoto.launch("image/*"));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_destination, container, false);
+    public void initializeWidgets() {
+        btnUploadCover = view.findViewById(R.id.btn_upload_cover);
     }
 }
