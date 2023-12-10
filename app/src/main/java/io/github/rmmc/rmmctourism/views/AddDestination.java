37fc3b840fd9e067839cbf4974c168ac09151f7a
ritchieculanag <ritchieculanag@gmail.com>
2023-12-10 20:47:58 +0800

    update: implement select multiple images

--- app/src/main/java/io/github/rmmc/rmmctourism/views/AddDestination.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/AddDestination.java
@@ -1,11 +1,15 @@
 package io.github.rmmc.rmmctourism.views;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -13,8 +17,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.github.rmmc.rmmctourism.R;
 import io.github.rmmc.rmmctourism.util.ActionInitializer;
@@ -22,12 +28,17 @@ import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
 public class AddDestination extends Fragment implements WidgetInitializer, ActionInitializer {
 
+    private static final int REQUEST_PERMISSION_CODE = 4283;
+
     private View view;
 
     private ImageView ivAddDestinationCover;
     private Button btnUploadCover;
 
-    private ActivityResultLauncher<String> arlTakePhoto;
+    private List<Uri> urisImg = new ArrayList<>();
+
+    private ActivityResultLauncher<String> selectDestinationCover;
+    private ActivityResultLauncher<String> requestPermissionLauncher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,12 +53,26 @@ public class AddDestination extends Fragment implements WidgetInitializer, Actio
 
     @Override
     public void initializeActions() {
-        arlTakePhoto = registerForActivityResult(
+        selectDestinationCover = registerForActivityResult(
                 new ActivityResultContracts.GetContent(),
                 uri -> ivAddDestinationCover.setImageURI(uri)
         );
 
-        btnUploadCover.setOnClickListener(e -> arlTakePhoto.launch("image/*"));
+        requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.GetMultipleContents(),
+                uris -> {
+                    uris.forEach(uri -> {
+                        Log.d("RITCHIE", uri.toString());
+                        urisImg.add(uri);
+                    });
+
+                    ivAddDestinationCover.setImageURI(urisImg.get(3));
+                }
+        );
+
+        btnUploadCover.setOnClickListener(e -> {
+            requestPermissionLauncher.launch("image/*");
+        });
     }
 
     @Override
