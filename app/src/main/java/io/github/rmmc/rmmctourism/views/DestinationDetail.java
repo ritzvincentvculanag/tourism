9e3cf5edf88e1b0db3e760a9a20c04555d467ccb
ritchieculanag <ritchieculanag@gmail.com>
2023-12-11 22:10:32 +0800

    update: add initial destination detail view

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/DestinationDetail.java
@@ -0,0 +1,17 @@
+package io.github.rmmc.rmmctourism.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.rmmc.rmmctourism.R;
+
+public class DestinationDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination_detail);
+    }
+}
+
