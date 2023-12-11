4880384880ae97ba6c208f1176471364fec7cf43
ritchieculanag <ritchieculanag@gmail.com>
2023-12-11 23:22:15 +0800

    update: finish initial layout of about fragment

--- app/src/main/java/io/github/rmmc/rmmctourism/fragments/About.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/fragments/About.java
@@ -7,60 +7,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link About#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class About extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class About extends Fragment implements WidgetInitializer, ActionInitializer {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View view;
 
-    public About() {
-        // Required empty public constructor
-    }
+    private TextView tvDescription;
+    private TextView tvPhone;
+    private TextView tvEmail;
+    private TextView tvFacebook;
+    private TextView tvInstagram;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment About.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static About newInstance(String param1, String param2) {
-        About fragment = new About();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        initializeWidgets();
+        initializeActions();
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void initializeActions() {
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+    public void initializeWidgets() {
+        tvDescription = view.findViewById(R.id.tv_destination_detail_description);
+        tvPhone = view.findViewById(R.id.tv_destination_detail_phone);
+        tvEmail = view.findViewById(R.id.tv_destination_detail_email);
+        tvFacebook = view.findViewById(R.id.til_add_destination_facebook);
+        tvInstagram = view.findViewById(R.id.tv_destination_detail_instagram);
     }
 }
