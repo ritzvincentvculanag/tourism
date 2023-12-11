6fc36e0274b3bcbb8b6f47d825042419c7bc19bc
ladengwapo <domingo.laden@gmail.com>
2023-12-11 10:58:16 +0800

    tangina matulog na ako haha . . tapus na yung logout tsaka sa imageview hahaha

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/adapter/ExploreSearchAdapter.java
@@ -0,0 +1,66 @@
+package io.github.rmmc.rmmctourism.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.model.Destination;
+import io.github.rmmc.rmmctourism.repository.ImageRepository;
+
+public class ExploreSearchAdapter extends RecyclerView.Adapter<ExploreSearchAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Destination> list;
+    private ImageRepository imageRepository;
+    public ExploreSearchAdapter(Context context, List<Destination> list){
+        this.context = context;
+        this.list = list;
+        this.imageRepository = new ImageRepository();
+    }
+    @NonNull
+    @Override
+    public ExploreSearchAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.layout_destination_search, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExploreSearchAdapter.MyViewHolder holder, int position) {
+        Destination destination = list.get(position);
+        holder.tvTitle.setText(destination.getName());
+        holder.tvDescription.setText(destination.getDescription());
+        holder.tvAddress.setText(destination.getAddress());
+        imageRepository.loadUploadedImage(destination.getDestinationId(), holder.ivCoverImg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivCoverImg;
+        private TextView tvTitle, tvDescription, tvAddress;
+        private Button btnViewDestination;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivCoverImg = itemView.findViewById(R.id.iv_destination_search_cover);
+            tvTitle = itemView.findViewById(R.id.tv_destination_search_title);
+            tvDescription = itemView.findViewById(R.id.tv_destination_search_description);
+            tvAddress = itemView.findViewById(R.id.tv_destination_search_address);
+            btnViewDestination = itemView.findViewById(R.id.btn_view_destination);
+        }
+    }
+}
--- app/src/main/java/io/github/rmmc/rmmctourism/fragments/Explore.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/fragments/Explore.java
@@ -1,28 +1,68 @@
 package io.github.rmmc.rmmctourism.fragments;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.adapter.ExploreAdapter;
+import io.github.rmmc.rmmctourism.adapter.ExploreSearchAdapter;
+import io.github.rmmc.rmmctourism.model.Destination;
+import io.github.rmmc.rmmctourism.repository.DestinationRepository;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.DestinationDataCallback;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
 public class Explore extends Fragment {
 
-
+    private RecyclerView rvSearchExplore;
+    private DestinationRepository destinationRepository;
+    private ExploreSearchAdapter exploreSearchAdapter;
+    private List<Destination> list;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        destinationRepository = new DestinationRepository();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        rvSearchExplore = view.findViewById(R.id.rv_explore_search);
+        populateData();
+        return view;
     }
+    private void populateData(){
+        destinationRepository.getDestination(new DestinationDataCallback<Destination>() {
+            @Override
+            public void onDataLoaded(List<Destination> t) {
+                list = t;
+                exploreSearchAdapter = new ExploreSearchAdapter(getContext(), list);
+                rvSearchExplore.setLayoutManager(new LinearLayoutManager(getActivity()));
+                rvSearchExplore.setAdapter(exploreSearchAdapter);
+                Log.d(TAG, "Loaded");
+
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+
+            }
+        });
+    }
+
+
+
 }
