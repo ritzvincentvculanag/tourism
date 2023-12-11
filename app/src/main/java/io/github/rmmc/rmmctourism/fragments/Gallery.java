e6cd5901646f544361a8b80961369a9ea7a216af
ritchieculanag <ritchieculanag@gmail.com>
2023-12-11 23:26:53 +0800

    update: finish layout for destination detail gallery

--- app/src/main/java/io/github/rmmc/rmmctourism/fragments/Gallery.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/fragments/Gallery.java
@@ -1,66 +1,53 @@
 package io.github.rmmc.rmmctourism.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.adapter.GalleryAdapter;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Gallery#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Gallery extends Fragment {
+public class Gallery extends Fragment implements WidgetInitializer, ActionInitializer {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private GalleryAdapter galleryAdapter;
+    private RecyclerView rvGallery;
+    private List<Uri> imgUris;
 
-    public Gallery() {
-        // Required empty public constructor
-    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Gallery.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Gallery newInstance(String param1, String param2) {
-        Gallery fragment = new Gallery();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        initializeWidgets();
+        initializeActions();
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void initializeActions() {
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    public void initializeWidgets() {
+        imgUris = new ArrayList<>();
+        galleryAdapter = new GalleryAdapter(imgUris);
+        rvGallery = view.findViewById(R.id.rv_destination_detail_gallery);
+        rvGallery.setAdapter(galleryAdapter);
+        rvGallery.setLayoutManager(new GridLayoutManager(getContext(), 3));
     }
 }
