eb0999cbf0e26d0f7e31da21743c0d0e660d0096
ritchieculanag <ritchieculanag@gmail.com>
2023-12-12 23:26:23 +0800

    update: finish layout of favorites and add initial implementation

--- app/src/main/java/io/github/rmmc/rmmctourism/views/Favorites.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/Favorites.java
@@ -3,64 +3,54 @@ package io.github.rmmc.rmmctourism.views;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import io.github.rmmc.rmmctourism.R;
+import io.github.rmmc.rmmctourism.adapter.ExploreSearchAdapter;
+import io.github.rmmc.rmmctourism.util.ActionInitializer;
+import io.github.rmmc.rmmctourism.util.OnDestinationClick;
+import io.github.rmmc.rmmctourism.util.WidgetInitializer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Favorites#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Favorites extends Fragment {
+public class Favorites extends Fragment implements WidgetInitializer, ActionInitializer, OnDestinationClick {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ExploreSearchAdapter adapter;
+    private RecyclerView favorites;
 
-    public Favorites() {
-        // Required empty public constructor
-    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_favorites, container, false);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Favorites.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Favorites newInstance(String param1, String param2) {
-        Favorites fragment = new Favorites();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        initializeWidgets();
+        initializeWidgets();
+
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void initializeWidgets() {
+        adapter = new ExploreSearchAdapter(getContext(), new ArrayList<>());
+        favorites = view.findViewById(R.id.rv_favorites);
+        favorites.setAdapter(adapter);
+        favorites.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    public void initializeActions() {
+        // TODO: Ikaw na bahala ano gawin mo dito
     }
+
+    @Override
+    public void onDestinationClick(int position) {
+        // TODO: implement on view destination click
+    }
+
 }
