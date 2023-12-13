c4a74566186060312cc37859be500334679dcde3
ritchieculanag <ritchieculanag@gmail.com>
2023-12-13 18:10:39 +0800

    update: add initial update email form

---
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/UpdateEmail.java
@@ -0,0 +1,16 @@
+package io.github.rmmc.rmmctourism.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.rmmc.rmmctourism.R;
+
+public class UpdateEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_email);
+    }
+}
