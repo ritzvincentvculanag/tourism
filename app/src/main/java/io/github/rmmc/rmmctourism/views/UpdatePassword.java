ce97c90ec17b9a06425a800159b24cfa20b9a450
ritchieculanag <ritchieculanag@gmail.com>
2023-12-13 18:33:47 +0800

    update: add initial implementation of update password button

--- app/src/main/java/io/github/rmmc/rmmctourism/views/UpdatePassword.java
+++ app/src/main/java/io/github/rmmc/rmmctourism/views/UpdatePassword.java
@@ -3,16 +3,29 @@ package io.github.rmmc.rmmctourism.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import io.github.rmmc.rmmctourism.R;
 
 public class UpdatePassword extends AppCompatActivity {
 
-    private
+    private TextInputLayout oldPassword;
+    private TextInputLayout newPassword;
+    private TextInputLayout confirmPassword;
+
+    private Button updatePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_password);
+
+        oldPassword = findViewById(R.id.til_update_password_old);
+        newPassword = findViewById(R.id.til_update_password_new);
+        confirmPassword = findViewById(R.id.til_update_password_confirm);
+
+        updatePassword = findViewById(R.id.btn_update_password);
     }
 }
